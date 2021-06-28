0c8878d3f762638f6341659fc0fffba93cc6b01e
juanBlancoSophos <juan.blanco@sophossolutions.com>
2021-06-28 11:38:34 -0500

    Actualizacion Juan Miguel

--- ScTestJava/lib/src/test/java/com/sophos/semillero/stepdefinitions/LoginStepDefinitions.java
+++ ScTestJava/lib/src/test/java/com/sophos/semillero/stepdefinitions/LoginStepDefinitions.java
@@ -1,5 +1,37 @@
 package com.sophos.semillero.stepdefinitions;
 
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.serenitybdd.screenplay.actions.Open;
+import net.serenitybdd.screenplay.actors.OnlineCast;
+
+import static net.serenitybdd.screenplay.actors.OnStage.setTheStage;
+import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
+
+import com.sophos.semillero.tasks.Login;
+
 public class LoginStepDefinitions {
 
+	@Given("Deseo ir a la pagina de {string}")
+	public void deseoIrALaPaginaDe(String strUrl) {
+	    theActorCalled("Juan Miguel").wasAbleTo(Open.url(strUrl));
+	}
+
+	@When("Ingreso nombre de usuario {string} y password {string}")
+	public void ingresoNombreDeUsuarioYPassword(String strUser, String strPassword) {
+		theActorCalled("Juan Miguel").wasAbleTo(Login.withCredential(strUser, strPassword));
+	}
+
+	@Then("Valido que el tiitulo sea {string}")
+	public void validoQueElTiituloSea(String string) {
+	    System.out.println("Then");
+	}
+	
+	@Before()
+	public void setup() {
+		setTheStage(new OnlineCast());
+	}
+	
 }
